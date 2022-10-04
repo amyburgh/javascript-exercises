85a16e731ae656feb5127b2293991c009752f18d
amyburgh <air.myburgh@gmail.com>
2022-10-04 15:33:54 +0200

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const temp = string
+        .toLowerCase()
+        .split('')
+        .filter(char => char.match(/[a-z]/i))
+        .join('');
+    return temp === temp.split('').reverse().join('');
 };
 
 // Do not edit below this line
