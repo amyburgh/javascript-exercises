c1ea2012b39c024103352e740e72689ceb265f0c
amyburgh <air.myburgh@gmail.com>
2022-10-04 17:50:27 +0200

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    return people.reduce((oldest, person) => {
+        let a = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let b = getAge(person.yearOfBirth, person.yearOfDeath);
+        return a > b ? oldest : person;
+    });
 };
+
+const getAge = function (birth, death) {
+    if (!death)
+        death = new Date().getFullYear();
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
